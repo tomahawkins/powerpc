4b2c463a0c0d38f23f969730e1a1718fe2da09f3
Tom Hawkins <tomahawkins@gmail.com>
2010-02-26 20:36:59 +0000

    simplified simulator

--- Language/PowerPC/Simulator.hs
+++ Language/PowerPC/Simulator.hs
@@ -1,4 +1,4 @@
--- | General tools for PowerPC programs.
+-- | PowerPC instruction set simulation.
 module Language.PowerPC.Simulator
   ( Memory (..)
   , simulate
@@ -6,87 +6,118 @@ module Language.PowerPC.Simulator
 
 import Control.Monad
 import Data.Bits
-import qualified Data.ByteString as BS
 import qualified Data.Map as M
 import Data.Word
 import Text.Printf
 
-import Language.PowerPC.Instruction
-import Language.PowerPC.Disassembler
+import Language.PowerPC.Opcode
 
-data Machine = Machine
-  { program :: M.Map Word64 (Word32, Instruction)
-  , pc      :: Word64
-  , lr      :: Word64
-  , ctr     :: Word64
-  , gprs    :: [Word64]
-  , xer     :: Word64
-  , cr      :: Word32
-  , ea      :: Word64
-  , memory  :: Memory
-  }
-
+-- | Memory interface.
 data Memory = Memory
   { load  :: Word64 -> Int -> IO [Word8]
   , store :: Word64 -> [Word8] -> IO ()
   }
 
-simulate :: Word64 -> Word64 -> BS.ByteString -> Memory -> IO ()
-simulate base start p m = loop Machine
-  { program = M.fromList $ zip [fromIntegral base, fromIntegral base + 4 ..] $ disassemble p
-  , memory = m
-  , pc      = start
+data Machine = Machine
+  { pc      :: Word64
+  , lr      :: Word64
+  , ctr     :: Word64
+  , gprs    :: [Word64]
+  , xer     :: Word64
+  , cr      :: Word32
+  }
+
+-- | Run simulation given memory interface, base address of program, starting address of program, and the program instructions.
+simulate :: Memory -> Word64 -> Word64 -> [Word32] -> IO ()
+simulate memory base start p = loop Machine
+  { pc      = start
   , lr      = 0
   , ctr     = 0
   , gprs    = replicate 32 0
   , xer     = 0
   , cr      = 0
-  , ea      = 0
   }
   where
 
+  program = M.fromList $ zip [fromIntegral base, fromIntegral base + 4 ..] p
+
   loop :: Machine -> IO ()
   loop m = when (not $ done m) $ do
-    printf "program counter = 0x%08X\n" (pc m)
-    step m >>= loop
+    n <- step memory (program M.! pc m) m
+    when (pc m + 4 /= pc n) $ printf "branched to: 0x%08X\n" $ pc n
+    loop n
 
   done :: Machine -> Bool
-  done m | pc m < base || pc m > base + (fromIntegral (BS.length p) + 4) = error $ printf "program counter out of range: 0x%08X" $ pc m
-         | otherwise                                                     = pc m == base + (fromIntegral $ BS.length p)
+  done m | pc m < base || pc m > base + (fromIntegral (length p) + 4) = error $ printf "program counter out of range: 0x%08X" $ pc m
+         | otherwise                                                  = pc m == base + (fromIntegral $ length p)
 
-step :: Machine -> IO Machine
-step m = case program m M.! pc m of
-  (_, IUnknown opcd xo) -> error $ printf "unknown instruction:  address: 0x%08X  opcode: %d  extended opcode: %d" (pc m) opcd xo
-  (i, I _ _ _ a) -> foldM (act i) m (if null [ () | PC := _ <- a ] then a ++ [PC := Reg PC + 4] else a)
+step :: Memory -> Word32 -> Machine -> IO Machine
+step memory instr m = case opcode instr of
 
-act :: Word32 -> Machine -> Action -> IO Machine
-act i m a = case a of
-  r := e -> case r of
-    PC  -> return m { pc  = eval e }
-    -- CR  -> return m { cr  = fromIntegral $ eval e }
-    LR  -> return m { lr  = eval e }
-    CTR -> return m { ctr = eval e }
-    XER -> return m { xer = eval e }
-    RA  -> return m { gprs = replace (fromIntegral $ field 11 15) (eval e) (gprs m) }
-    RT  -> return m { gprs = replace (fromIntegral $ field  6 10) (eval e) (gprs m) }
-    EA  -> return m { ea = eval e }
-  Load n -> do d <- load (memory m) (ea m) n; return m { gprs = replace (fromIntegral $ field  6 10) (fromBytes d) (gprs m) }
-  Store n d -> store (memory m) (ea m) (toBytes n $ eval d) >> return m
-  AddF flags a b c -> return m { gprs = replace (fromIntegral $ field  6 10) (eval a + eval b + eval c) (gprs m) } --XXX
+  --[ I "add"      31 266 [ AddF [(Rc, CR 0), (OE, OV)] (Reg RA) RB 0 ]
+
+  -- addi
+  (14,   0) -> return $ set n rti $ if rai == 0 then si else si + ra
+
+  -- addis
+  (15,   0) -> return $ set n rti $ if rai == 0 then shiftL si 16 else shiftL si 16 + ra
+
+  -- b
+  (18,   0) -> return m { pc = li + if aa then 0 else pc m, lr = if lk then pc m + 4 else lr m }
+
+  -- lwzu
+  (33,   0) -> do
+    a <- load memory ea 4
+    return $ set (set n rti (fromBytes a)) rai ea
+    where
+    ea = ra + d
+
+  -- mfmsr
+  (31,  83) -> printf "warning (mfmsr): instruction ignored\n" >> return n
+
+  -- mfspr
+  (31, 339) -> case spr of
+    0x01 -> return $ set n rti $ xer m
+    0x08 -> return $ set n rti $ lr  m
+    0x09 -> return $ set n rti $ ctr m
+    _ -> printf "warning (mfspr): unknown code: 0x%X\n" spr >> return n
+
+  -- mtmsr
+  (31, 146) -> printf "warning (mtmsr): instruction ignored\n" >> return n
+
+  -- mtspr
+  (31, 467) -> case spr of
+    0x01 -> return n { xer = rs }
+    0x08 -> return n { lr  = rs }
+    0x09 -> return n { ctr = rs }
+    _ -> printf "warning (mtspr): unknown code: 0x%X\n" spr >> return n
+
+  -- ori
+  (24,   0) -> return $ set n rai $ rs .|. ui
+
+  -- sth
+  (44,   0) -> store memory ea (toBytes 2 rs) >> return n
+    where
+    ea = d + if rai == 0 then 0 else ra
+
+  --, I "sthu"     45   0 [ EA := Reg RA + D, Store 2 RS, RA := Reg EA ]
+
+  -- stw
+  (36,   0) -> store memory ea (toBytes 4 rs) >> return n
+    where
+    ea = d + if rai == 0 then 0 else ra
+  
+  --, I "stwu"     37   0 [ EA := Reg RA + D, Store 4 RS, RA := Reg EA ]
+
+  (a, b) -> error $ printf "unknown instruction:  address: 0x%08X  instruction: 0x%08X  opcd: %d  xo: %d" (pc m) instr a b
+
   where
 
-  toBytes :: Int -> Word64 -> [Word8]
-  toBytes n _ | n <= 0 = []
-  toBytes n d = fromIntegral (shiftR d ((n - 1) * 8)) : toBytes (n - 1) d
-
-  fromBytes :: [Word8] -> Word64
-  fromBytes = f . reverse
-    where
-    f [] = 0
-    f (a:b) = fromIntegral a .|. shiftL (f b) 8
-
   field :: Int -> Int -> Word64
-  field h l = shiftR (fromIntegral i) (31 - l) .&. foldl setBit 0 [0 .. l - h] 
+  field h l = shiftR (fromIntegral instr) (31 - l) .&. foldl setBit 0 [0 .. l - h] 
+
+  bit :: Int -> Bool
+  bit n = testBit instr (31 - n)
 
   exts :: Int -> Word64 -> Word64
   exts n w = if testBit w (63 - n) then foldl setBit 0 [63 - n + 1 .. 63]  .|. w else w
@@ -94,43 +125,46 @@ act i m a = case a of
   exts32 :: Int -> Word64 -> Word64
   exts32 n =  exts (32 + n)
 
-  eval :: E -> Word64
-  eval a = case a of
-    Const a -> a
-    Reg a -> case a of
-      PC  -> pc  m
-      -- CR  -> fromIntegral $ cr  m
-      LR  -> lr  m
-      CTR -> ctr m
-      XER -> xer m
-      RA  -> gprs m !! fromIntegral (field 11 15)
-      RT  -> gprs m !! fromIntegral (field  6 10)
-      EA  -> ea m
-    Add a b -> eval a + eval b
-    Sub a b -> eval a + eval b
-    Mul a b -> eval a * eval b  --XXX Signedness?
-    Not a   -> complement $ eval a
-    And a b -> eval a .&. eval b
-    Or  a b -> eval a .|. eval b
-    Eq  a b -> if eval a == eval b then 1 else 0
-    Lt  a b -> if eval a <  eval b then 1 else 0  --XXX Signedness?
-    Shift a n -> shift (eval a) n
-    If a b c -> if eval a /= 0 then eval b else eval c
-    Exts n a -> exts n $ eval a
-    AA   -> field 30 30
-    D    -> exts32 16 $ field 16 31
-    LI   -> exts32  6 $ field 6 31 .&. complement 0x3
-    LK   -> field 31 31
-    OE   -> field 21 21
-    RAI  -> field 11 15
-    RB   -> gprs m !! fromIntegral (field 16 20)
-    Rc   -> field 31 31
-    RS   -> gprs m !! fromIntegral (field  6 10)
-    SI   -> exts32 16 $ field 16 31
-    SPR  -> shiftL (field 16 20) 5 .|. field 11 15
-    UI   -> field 16 31
-  
-replace :: Int -> a -> [a] -> [a]
+  n = m { pc = pc m + 4 }
+
+  set :: Machine -> Word64 -> Word64 -> Machine
+  set m i v = m { gprs = replace i v $ gprs m }
+
+  aa   = bit 30
+  d    = exts32 16 $ field 16 31
+  li   = exts32  6 $ field 6 31 .&. complement 0x3
+  lk   = bit 31
+  oe   = bit 21
+  ra   = gprs m !! fromIntegral rai
+  rai  = field 11 15
+  rb   = gprs m !! fromIntegral rbi
+  rbi  = field 16 20
+  rc   = bit 31
+  rs   = gprs m !! fromIntegral rsi
+  rsi  = field 6 10
+  rti  = field 6 10
+  si   = exts32 16 $ field 16 31
+  spr  = shiftL (field 16 20) 5 .|. field 11 15
+  ui   = field 16 31
+
+
+
+
+
+
+
+replace :: Integral b => b -> a -> [a] -> [a]
 replace _ a [] = [a]
 replace i a (b:c) | i <= 0    = a : c
                   | otherwise = b : replace (i - 1) a c
+
+toBytes :: Int -> Word64 -> [Word8]
+toBytes n _ | n <= 0 = []
+toBytes n d = fromIntegral (shiftR d ((n - 1) * 8)) : toBytes (n - 1) d
+
+fromBytes :: [Word8] -> Word64
+fromBytes = f . reverse
+  where
+  f [] = 0
+  f (a:b) = fromIntegral a .|. shiftL (f b) 8
+
--- Test.hs
+++ Test.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Bits
 import qualified Data.ByteString as BS
 import Data.IORef
 import qualified Data.Map as M
@@ -12,7 +13,12 @@ main :: IO ()
 main = do
   p <- BS.readFile "test"
   m <- memory p
-  simulate 0 0x100 p m
+  simulate m 0 0x100 $ pack $ BS.unpack p
+
+pack :: [Word8] -> [Word32]
+pack [] = []
+pack (a:b:c:d:e) = (shiftL (fromIntegral a) 24 .|. shiftL (fromIntegral b) 16 .|. shiftL (fromIntegral c) 8 .|. fromIntegral d) : pack e
+pack _ = error "disassemble: byte string not divisable by 4"
 
 memory :: BS.ByteString -> IO Memory
 memory p = do
@@ -25,7 +31,7 @@ memory p = do
     | addr >= 0x3F8000 && addr < 0x400000 = do
       ram <- readIORef ram
       return $ map (ram M.!) addrs
-    | otherwise = printf "load  0x%08X %d bytes\n" addr bytes
+    | otherwise = printf "load (%s) 0x%08X %d bytes\n" (section addr) addr bytes
     where
     addrs = [addr .. addr + fromIntegral bytes - 1]
 
@@ -34,5 +40,11 @@ memory p = do
     | addr >= 0x3F8000 && addr < 0x400000 = do
       r <- readIORef ram
       writeIORef ram $ foldl (\ m (addr, byte) -> M.insert addr byte m) r $ zip [addr ..] bytes
-    | otherwise = printf "store 0x%08X 0x" addr >> mapM_ (printf "%02X") bytes >> printf "\n"
-  
+    | otherwise = printf "store (%s) 0x%08X 0x" (section addr) addr >> mapM_ (printf "%02X") bytes >> printf "\n"
+ 
+  section addr
+    | addr >= 0x30000000 && addr < 0x30000000 + 16384 = "EEPROM_SPACE"
+    | addr >= 0x00302000 && addr < 0x00302000 + 2048  = "RUNTIME_STARTUP_STACK(TPU)"
+    | addr >= 0x00304100 && addr < 0x00304100 + 256   = "TPU_A_SPACE"
+    | addr >= 0x00304500 && addr < 0x00304500 + 256   = "TPU_B_SPACE"
+    | otherwise                                       = "UNKNOWN"
